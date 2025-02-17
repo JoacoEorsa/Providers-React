0074f72ab9796207df57c859e3d92ee5ed16ae73
Sergio Ojeda <soj3da@gmail.com>
2025-02-16 23:41:20 -0300

    feat: add tailwind css

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from '@tailwindcss/vite'
 
 import path from "path";
 
@@ -21,7 +22,7 @@ export default defineConfig({
 	},
 
 	// Plugins
-	plugins: [react()],
+	plugins: [react(), tailwindcss()],
 
 	// Resolve configuration
 	resolve: {

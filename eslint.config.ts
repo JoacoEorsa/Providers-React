39b5be8de562016351888619b3ee35fce8cde3eb
Sebastian Lagomarsino <84023302+slagomarsino-dev@users.noreply.github.com>
2025-02-24 11:20:17 -0300

    Add eslint rules and change error severity (#6)
    * Add eslint rules.
    * Change eslint rules errors severity to improve developers experience.
    * Update dependencies.

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,4 +1,5 @@
 import js from '@eslint/js';
+import stylisticJs from '@stylistic/eslint-plugin-js';
 import type { ESLint, Linter } from 'eslint';
 import preferArrowFunctions from 'eslint-plugin-prefer-arrow-functions';
 import react from 'eslint-plugin-react';
@@ -6,6 +7,7 @@ import simpleImportSort from 'eslint-plugin-simple-import-sort';
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment -- The type doesn't exist.
 // @ts-expect-error
 import sortDestructureKeys from 'eslint-plugin-sort-destructure-keys';
+import unusedImports from 'eslint-plugin-unused-imports';
 import globals from 'globals';
 import ts from 'typescript-eslint';
 
@@ -20,11 +22,11 @@ const config: Linter.Config[] = [
     rules: {
       ...js.configs.recommended.rules,
       'arrow-body-style': ['error', 'always'],
-      curly: 'error',
-      'no-console': ['warn'],
+      curly: 'warn',
+      'no-console': 'warn',
       'no-extra-boolean-cast': 'error',
       'no-unneeded-ternary': 'error',
-      'object-shorthand': 'error'
+      'object-shorthand': 'warn'
     }
   },
 
@@ -34,7 +36,8 @@ const config: Linter.Config[] = [
     rules: {
       ...react.configs.recommended.rules,
       'react/hook-use-state': 'warn',
-      'react/jsx-sort-props': ['error', { shorthandLast: true }],
+      'react/jsx-no-useless-fragment': 'warn',
+      'react/jsx-sort-props': ['warn', { shorthandLast: true }],
       'react/react-in-jsx-scope': 'off'
     }
   },
@@ -42,21 +45,32 @@ const config: Linter.Config[] = [
   // TypeScript rules
   ...([
     ...ts.configs.recommended,
-    { rules: { '@typescript-eslint/method-signature-style': ['error', 'property'] } }
+    {
+      rules: {
+        '@typescript-eslint/method-signature-style': ['error', 'property'],
+        '@typescript-eslint/no-unused-vars': 'off'
+      }
+    }
   ] as Linter.Config[]),
 
   // Custom plugins rules
   {
     plugins: {
+      '@stylistic/js': stylisticJs,
       'prefer-arrow-functions': preferArrowFunctions as ESLint.Plugin,
       'simple-import-sort': simpleImportSort,
-      'sort-destructure-keys': sortDestructureKeys
+      'sort-destructure-keys': sortDestructureKeys,
+      'unused-imports': unusedImports
     },
     rules: {
+      '@stylistic/js/no-multi-spaces': 'warn',
+      '@stylistic/js/no-multiple-empty-lines': ['warn', { max: 1 }],
+      '@stylistic/js/no-trailing-spaces': 'warn',
+      '@stylistic/js/object-curly-spacing': ['warn', 'always'],
       'prefer-arrow-functions/prefer-arrow-functions': 'error',
-      'simple-import-sort/exports': 'error',
+      'simple-import-sort/exports': 'warn',
       'simple-import-sort/imports': [
-        'error',
+        'warn',
         {
           groups: [
             // Packages. `react` related packages come first.
@@ -83,7 +97,12 @@ const config: Linter.Config[] = [
           ]
         }
       ],
-      'sort-destructure-keys/sort-destructure-keys': 'error'
+      'sort-destructure-keys/sort-destructure-keys': 'warn',
+      'unused-imports/no-unused-imports': 'warn',
+      'unused-imports/no-unused-vars': [
+        'warn',
+        { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' }
+      ]
     }
   }
 ];

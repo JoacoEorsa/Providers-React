1f4ac1bcf19f630acd7a82bb98740c971e4ca612
Hernán Yáñez <142254889+hyanez-lightit@users.noreply.github.com>
2025-03-07 13:52:31 -0300

    feat: restrict bundle analyzer for local dev (#33)

--- vite.config.ts
+++ vite.config.ts
@@ -3,24 +3,26 @@ import tailwindcss from '@tailwindcss/vite';
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 import viteReact from '@vitejs/plugin-react';
 import path from 'path';
-import { type UserConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import { analyzer } from 'vite-bundle-analyzer';
 
-// https://vitejs.dev/config/
-export default {
-  plugins: [
-    TanStackRouterVite(),
-    viteReact(),
-    tailwindcss(),
-    analyzer(),
-    // Put the Sentry vite plugin after all other plugins
-    sentryVitePlugin({
-      org: process.env.VITE_SENTRY_ORG,
-      project: process.env.VITE_SENTRY_PROJECT,
+export default defineConfig(({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
-      // Auth tokens can be obtained from https://sentry.io/orgredirect/organizations/:orgslug/settings/auth-tokens/
-      authToken: process.env.VITE_SENTRY_AUTH_TOKEN,
-    }),
-  ],
-  resolve: { alias: { '@': path.resolve(__dirname, './src') } },
-} satisfies UserConfig;
+  return {
+    plugins: [
+      TanStackRouterVite(),
+      viteReact(),
+      tailwindcss(),
+      ...(process.env.VITE_APP_ENV === 'local' ? [analyzer()] : []),
+      sentryVitePlugin({
+        org: process.env.VITE_SENTRY_ORG,
+        project: process.env.VITE_SENTRY_PROJECT,
+
+        // Auth tokens can be obtained from https://sentry.io/orgredirect/organizations/:orgslug/settings/auth-tokens/
+        authToken: process.env.VITE_SENTRY_AUTH_TOKEN,
+      }),
+    ],
+    resolve: { alias: { '@': path.resolve(__dirname, './src') } },
+  };
+});

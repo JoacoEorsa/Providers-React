d95d686a5ef9ac8fd6270d12ecfe3b1cbd5aa549
juancatenalightit <juan.catena@lightit.io>
2025-04-25 12:21:57 -0300

    [NO-TASK] Fix checkbox type (#83)

--- src/components/ui/checkbox/checkbox.tsx
+++ src/components/ui/checkbox/checkbox.tsx
@@ -14,7 +14,7 @@ const checkboxVariants = tv({
 
 const { icon, indicator, root } = checkboxVariants();
 
-export type CheckboxProps = {} & ComponentProps<typeof CheckboxPrimitive.Root>;
+export type CheckboxProps = ComponentProps<typeof CheckboxPrimitive.Root>;
 
 const Checkbox = ({ className, ...props }: CheckboxProps) => {
   return (

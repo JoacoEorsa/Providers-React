c0928999cafc7200b868193bd9e4f3e2457e8d50
JoacoErosa <joaquin.erosa@lightit.io>
2026-06-02 10:39:23 -0300

    refactor(providers): use deepSnakeKeys for param serialization

--- react-template/src/services/providers/api.ts
+++ react-template/src/services/providers/api.ts
@@ -1,3 +1,4 @@
+import { deepSnakeKeys } from "string-ts";
 import { z } from "zod";
 
 import { publicApi } from "@/config/api";
@@ -6,38 +7,8 @@ import type { RequestParams } from "@/services/types";
 import { getProviderSchema } from "./schemas";
 import type { Provider, ProvidersFilter } from "./types";
 
-const camelToSnake = (key: string) => {
-  return key.replaceAll(/[A-Z]/g, (letter) => {
-    return `_${letter.toLowerCase()}`;
-  });
-};
-
-const serializeProvidersParams = (params: RequestParams<ProvidersFilter>) => {
-  const search = new URLSearchParams();
-
-  if (params.page != null) {
-    search.set("page", String(params.page));
-  }
-
-  if (params.filter) {
-    for (const [key, value] of Object.entries(params.filter)) {
-      if (value === undefined || value === null || value === "") {
-        continue;
-      }
-      search.set(`filter[${camelToSnake(key)}]`, String(value));
-    }
-  }
-
-  return search.toString();
-};
-
 export const getProviders = async (params: RequestParams<ProvidersFilter>) => {
-  const response = await publicApi.get("providers", {
-    params,
-    paramsSerializer: () => {
-      return serializeProvidersParams(params);
-    },
-  });
+  const response = await publicApi.get("providers", { params: deepSnakeKeys(params) });
 
   return parsePaginatedResponse(z.array(getProviderSchema()), response.data);
 };
